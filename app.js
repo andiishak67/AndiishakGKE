914b7600f825813cfa01a072f12fc63420bfffbe
andiishak67 <91652502+andiishak67@users.noreply.github.com>
2023-04-06 13:20:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Andi Ishak Mangeragi\n')
+    res.send('Andi Ishak\n')
     res.send('51021801\n')
     res.send('Sistem Informasi\n')
 })
